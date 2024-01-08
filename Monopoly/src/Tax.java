45f0bf3e13dcbf6d2fcef390517dbc3d8603aac4
NguyenVuMinhNhat <154681403+NguyenVuMinhNhat@users.noreply.github.com>
2024-01-08 13:59:02 +0700

    Update Tax.java

--- Monopoly/src/Tax.java
+++ Monopoly/src/Tax.java
@@ -1,30 +1,32 @@
-public class Tax extends Square{
-   
-    
+public class Tax extends Square {
+
     private int totalTax;
+
     public Tax() {
         super("Tax", 37, SquareType.TAX);
-        
+
     }
 
-   
+    public void setTotalTax(int totalTax) {
+        this.totalTax = totalTax;
+    }
 
     public int getTotalTax() {
         return totalTax;
     }
-    
+
     public int calculateTotalTax(Player player, Board board) {
         int totalTax = 0;
         for (int i = 0; i < player.getOwnedProperty().size(); i++) {
             if (board.timer.getElapsedTime() <= 5) {
-                totalTax += 5 / 100 * player.getCash() + 5 / 100 * player.calculateTotalPropertyValue();
+                totalTax += 5 * (player.getCash() + player.calculateTotalPropertyValue()) / 100;
             } else if (board.timer.getElapsedTime() > 5 && board.timer.getElapsedTime() < 10) {
-                totalTax += 1 / 10 * player.getCash() + 1 / 10 * player.calculateTotalPropertyValue();
+                totalTax += (player.getCash() + player.calculateTotalPropertyValue()) / 10;
             } else {
-                totalTax += 1 / 10 * player.getCash() + 15 / 100 * player.calculateTotalPropertyValue();
+                totalTax += (player.getCash() / 10) + (player.calculateTotalPropertyValue() * 15) / 100;
             }
         }
         return totalTax;
     }
-    
+
 }
