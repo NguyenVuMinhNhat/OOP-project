b2913e31fd53d62bbe087a307b2e9e3f1682a472
NguyenVuMinhNhat <154681403+NguyenVuMinhNhat@users.noreply.github.com>
2024-01-09 00:04:20 +0700

    Add files via upload

---
+++ Monopoly/src/Square.java
@@ -0,0 +1,44 @@
+public class Square {
+    private String name;
+    private int location;
+    private SquareType type;
+
+    public Square(String name){
+        this.name = name;
+    }
+
+    public Square(String name, int location){
+        this.name = name;
+        this.location = location;
+    }
+
+    public Square(String name, int location, SquareType type) {
+        this.name = name;
+        this.location = location;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void setLocation(int location) {
+        this.location = location;
+    }
+
+    public SquareType getType() {
+        return type;
+    }
+
+    public void setType(SquareType type) {
+        this.type = type;
+    }
+}
